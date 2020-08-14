1fb654354e0feb88080f5f8783bbed50eab47515
Matteo Cagno <matteo.cagno@string.it>
2020-08-14 09:28:11 +0200

    no message

---
+++ MyTestLibrary/Summer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyTestLibrary
+{
+    public class Summer
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Rum(int a)
+        {
+            //Prova
+            return a;
+        }
+    }
+}
